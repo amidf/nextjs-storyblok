19d1de0fa97cd802daf04103d27432fe77dfacd2
Дима Федин <fedinamid@192-168-0-100.local>
2021-12-14 07:43:57 +0300

    add storyblok

---
+++ components/Teaser.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { sbEditable } from "@storyblok/storyblok-editable";
+
+const Teaser = ({ blok }) => {
+  return (
+    <div {...sbEditable(blok)} key={blok._uid} className="bg-white-half">
+      <div className="pb-6 pt-16 container mx-auto">
+        <h2 className="text-6xl font-bold font-serif text-primary mb-4">
+          {blok.headline}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Teaser;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  i18n: {
+    localeDetection: false,
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+};
